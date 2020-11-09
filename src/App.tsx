d867a4697749f1d820b193b1ffd9c32f3ec32831
Dexer <norton@e1.ru>
2020-11-09 20:07:37 +0500

    add wrong url handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import { useRoutes } from 'hookrouter';
 import routes from './routes';
 import './style.css';
 
 const App = () => {
   const match = useRoutes(routes);
-  return match;
+  return match || <div>404 page</div>;
 };
 export default App;
