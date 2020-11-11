f2ab34072bce16f92b774ecca3097849f0297cec
Dexer <norton@e1.ru>
2020-11-11 22:48:42 +0500

    add get pokemons data from api

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -4,28 +4,42 @@ import style from './Pokedex.module.scss';
 import Layout from '../../components/Layout';
 import Heading from '../../components/Heading';
 import PokemonCard from '../../components/PokemonCard';
-import data, { API } from './data';
+import { API } from './data';
 
-const PokedexPage: React.FC = () => {
+const usePokemons = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [pokemonsCount, setPokemonsCount] = useState(0);
+  const [data, setData] = useState({} as API);
   const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     setIsLoading(true);
     const fetchData = async () => {
       try {
-        const response = await fetch('http://zar.hosthot.ru/api/v1/pokemons');
+        const response = await fetch('http://zar.hosthot.ru/api/v1/pokemons?limit=15');
         const body: API = await response.json();
-        setPokemonsCount(body.total);
-        setIsLoading(false);
+        setData(body);
       } catch (err) {
         setErrorMessage(err.message);
+      } finally {
         setIsLoading(false);
       }
     };
     fetchData();
   }, []);
+
+  return {
+    isLoading,
+    data,
+    errorMessage,
+  };
+};
+
+const PokedexPage: React.FC = () => {
+  const {
+    data: { count, pokemons },
+    isLoading,
+    errorMessage,
+  } = usePokemons();
 
   if (isLoading) {
     return <div>loading...</div>;
@@ -39,10 +53,10 @@ const PokedexPage: React.FC = () => {
     <div className={style.pokedex}>
       <Layout>
         <Heading className={style.pokedex__header} as="h3">
-          {pokemonsCount} <b>Pokemons</b> for you to choose your favorite
+          {count} <b>Pokemons</b> for you to choose your favorite
         </Heading>
         <div className={style['pokedex__pokemon-list']}>
-          {data.map((pokemonInfo, index) => {
+          {pokemons.map((pokemonInfo, index) => {
             const classes = {
               [style['pokedex__pokemon-item']]: true,
               [style['pokedex__pokemon-item--middle-item']]: (index + 1) % 3 === 2,
