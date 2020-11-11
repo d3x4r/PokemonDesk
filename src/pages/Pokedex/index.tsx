5faf22c94070cec449de0d1df1d1efb5aa513c24
Dexer <norton@e1.ru>
2020-11-11 22:27:11 +0500

    add dynamic pokedex title

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import cn from 'classnames';
 import style from './Pokedex.module.scss';
 import Layout from '../../components/Layout';
+import Heading from '../../components/Heading';
 import PokemonCard from '../../components/PokemonCard';
-import data from './data';
+import data, { API } from './data';
 
 const PokedexPage: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pokemonsCount, setPokemonsCount] = useState(0);
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://zar.hosthot.ru/api/v1/pokemons');
+        const body: API = await response.json();
+        setPokemonsCount(body.total);
+        setIsLoading(false);
+      } catch (err) {
+        setErrorMessage(err.message);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+
+  if (errorMessage) {
+    return <div>{errorMessage}</div>;
+  }
+
   return (
     <div className={style.pokedex}>
       <Layout>
+        <Heading className={style.pokedex__header} as="h3">
+          {pokemonsCount} <b>Pokemons</b> for you to choose your favorite
+        </Heading>
         <div className={style['pokedex__pokemon-list']}>
           {data.map((pokemonInfo, index) => {
             const classes = {
