53d2566235e4e5e17dde6d26236393644596f04d
Dexer <norton@e1.ru>
2020-11-26 18:49:27 +0500

    add id of pokemon to pathname

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import useData from '../../hook/getData';
+import { PokemonInfoType } from '../Pokedex/data';
 
 export interface PokemonPageProps {
   id: string | number;
 }
 
 const PokemonPage = ({ id }: PokemonPageProps) => {
-  return <div>this is pokemon page {`${id}`}</div>;
+  const { data, isLoading } = useData<PokemonInfoType>('getPokemon', { id });
+
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+
+  return <div>this is pokemon page {`${data?.name}`}</div>;
 };
 
 export default PokemonPage;
--- src/utils/getUrlWithParamsConfig.ts
+++ src/utils/getUrlWithParamsConfig.ts
@@ -1,13 +1,29 @@
 import config from '../config';
 
 const getUrlWithParamsConfig = (endpoingConfig: string, query: object) => {
-  return {
+  const url = {
     ...config.client.server,
     ...config.client.endpoint[endpoingConfig as keyof typeof config.client.endpoint].uri,
-    query: {
-      ...query,
-    },
+    query: {},
   };
+
+  const pathname = (Object.keys(query) as Array<keyof typeof query>).reduce((acc, key) => {
+    if (acc.indexOf(`{${key}}`) !== -1) {
+      const result = acc.replace(`{${key}}`, query[key]);
+      // eslint-disable-next-line no-param-reassign
+      delete query[key];
+      return result;
+    }
+
+    return acc;
+  }, url.pathname);
+
+  url.pathname = pathname;
+
+  url.query = {
+    ...query,
+  };
+  return url;
 };
 
 export default getUrlWithParamsConfig;
